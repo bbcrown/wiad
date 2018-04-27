d8667692dd2fb05ee53620299c444efc9ec5c537
bnasr <bijan.s.nasr@gmail.com>
2018-04-27 14:28:20 -0700

    extra features

--- ui.R
+++ ui.R
@@ -1,5 +1,18 @@
+#######################################################################
+# The UI side for the TRIAD shiny app. 
+# 
+# The TRIAD app is developed and maintained by Bijan Seyednasrollah.
+#
+# Most recent release: https://github.com/bnasr/TRIAD
+#######################################################################
+
+
 fluidPage(
+  
   theme= shinytheme('slate'),
+  
+  shinyjs::useShinyjs(),
+  
   tags$head(
     tags$style(HTML("
     .shiny-output-error-validation {
@@ -7,6 +20,7 @@ fluidPage(
     }
     "))
   ),
+  
   titlePanel("TRIAD: Tree Ring Image Analysis and Dataset"),
   
   sidebarLayout(
@@ -14,7 +28,7 @@ fluidPage(
     sidebarPanel(
       
       fileInput("image", "Choose the image file",
-                multiple = TRUE,
+                multiple = FALSE,
                 accept = c("image/jpeg",
                            "image/png",
                            "image/tiff",
@@ -22,37 +36,87 @@ fluidPage(
       
       # Horizontal line ----
       tags$hr(),
+      
       textInput('ownerName', "Name", placeholder = 'Your name'),
+      
       textInput('ownerEmail', "Email address", placeholder = 'email address'),
+      
       textInput('spp', "Species", placeholder = 'What genus/species?'),
+      
       textInput('sampleDate', "Sample Date", placeholder = 'When was the sample collected?'),
+      
       textInput('sampleLoc', "Sample Location", placeholder = 'Where was the sample collected from?'),
+      
       textInput('sampleNote', "Sample note", placeholder = 'Any additional note?'),
       
       hr(),
+      
       actionButton('saveData', 'Save', width = '100%', icon = icon('save'))
     ),
     
     mainPanel(
       # Output: Data file ----
-      plotOutput("imageOrig"),
+      plotOutput("imageOrig", click = "orgPoint"),
+      
       hr(),
-      fluidRow(column(1, actionButton('Red', 'R', width = '100%')),
-               column(1, actionButton('Green', 'G', width = '100%')),
-               column(1, actionButton('Blue', 'B', width = '100%')),
-               column(1, actionButton('Hue', 'H', width = '100%')),
-               column(1, actionButton('Sat', 'S', width = '100%')),
-               column(1, actionButton('Value', 'V', width = '100%')),
-               column(1, actionButton('Bright', 'Bri', width = '100%')),
-               column(1, actionButton('Dark', 'Drk', width = '100%')),
-               column(1, actionButton('totBr', 'tBR', width = '100%')),
-               column(1, actionButton('Clusted', 'Clus', width = '100%')),
-               column(1, actionButton('Inverse', 'Inv', width = '100%')),
-               column(1, actionButton('Inverse', 'Inv', width = '100%'))
-               
+      
+      fluidRow(
+        column(3, 
+               actionButton('selRed', 'Red', width = '100%', icon=icon("bitbucket"), style="color: red; background-color: red; border-color: black;")
+        ),
+        
+        column(3, 
+               actionButton('selHue', 'Hue', width = '100%')
+        ),
+        
+        column(3,
+               actionButton('selBright', 'Brightness', width = '100%', icon=icon("sun-o"), style="color: yellow; background-color: yellow; border-color: black;")
+        ),
+        
+        column(3,
+               actionButton('selTotBr', 'Total Br.', width = '100%', icon=icon("bell"))
+        )
       ),
+      
+      fluidRow(
+        column(3, 
+               actionButton('selGreen', 'Green', width = '100%', icon=icon("bitbucket"), style="color: green; background-color: green; border-color: black;")
+        ),
+        
+        column(3, 
+               actionButton('selSat', 'Saturation', width = '100%')
+        ),
+        
+        column(3, 
+               actionButton('selDark', 'Darkness', width = '100%', icon=icon("moon-o"), style="color: black; background-color: black; border-color: black;")
+        ),
+        
+        column(3, 
+               actionButton('selCluster', 'Clus', width = '100%', icon=icon("bell"))
+        )
+      ),
+      
+      fluidRow(
+        column(3,
+               actionButton('selBlue', 'Blue', width = '100%', icon=icon("bitbucket"), style="color: blue; background-color: blue; border-color: black;")
+        ),
+        
+        column(3, 
+               actionButton('selValue', 'Value', width = '100%')
+        ),
+        
+        column(3, 
+               actionButton('selContrast', 'Contrast', width = '100%', icon=icon("moon"), style="color: gray; background-color: gray; border-color: black;")
+        ),
+        
+        column(3, 
+               actionButton('selInverse', 'Inv', width = '100%', icon=icon("bell"))
+        )
+      ),
+      
       br(),
-      plotOutput("imageProc")
+      
+      plotOutput("imageProc", click = "prcPoint")
       
     )
     
