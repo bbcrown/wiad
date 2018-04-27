9a25d4f0ba0d8a6f1e11d9fbcfeae3f2bee3364a
bnasr <bijan.s.nasr@gmail.com>
2018-04-27 14:28:54 -0700

    global libs added

--- global.R
+++ global.R
@@ -1,39 +1,43 @@
 #######################################################################
-# The global setup for the imageRun shiny app. 
+# The global setup for the TRIAD shiny app. 
 # 
-# The imageRun app is developed and maintained by Bijan Seyednasrollah.
-# The main initial development was done in November, 2017.
+# The TRIAD app is developed and maintained by Bijan Seyednasrollah.
 #
-# Most recent release: https://github.com/bnasr/imageRun
+# Most recent release: https://github.com/bnasr/TRIAD
 #######################################################################
 
 source('aux.R')
 
+
 list.of.packages <- c(
-  'rgdal',
+  'data.table',
+  'imager',
+  'jpeg',
+  'png',
   'shiny',
-  # 'shinyjs',
   'shinyFiles',
+  'shinyjs',
   'shinythemes',
+  'raster',
+  'rgdal',
   'rjson',
   'RJSONIO',
   'sp',
-  'rgdal',
-  'raster',
-  'jpeg',
-  'png',
   'tiff',
-  'tools',
-  'data.table'
+  'tools'
 )
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos='http://cran.rstudio.com/')
+
+if(length(new.packages)) 
+  install.packages(new.packages, 
+                   repos='http://cran.rstudio.com/')
 
 for(p in list.of.packages) library(p, character.only = T)
 
 LOCAL_RUN <- T
 if(system('hostname', intern=T)%in%c('phenocam')&
+   
    system('whoami', intern=T)%in%c('shiny')) LOCAL_RUN <- F
 
 PRINT_LOGS <- T
