b9f65123879a6f9047c8b0006c4944569a0111de
bnasr <bijan.s.nasr@gmail.com>
2021-01-28 16:02:53 -0700

    base for test rgb

--- test/testthat/testwiad.R
+++ test/testthat/testwiad.R
@@ -1,5 +1,11 @@
 context("testwiad")
 
-test_that("test ...", {
-  #expect_equal(,)
-})
+set.seed(2020)
+
+w = 10
+h = 5
+
+rgbArray = array(data = runif(w * h * 3), 
+                 dim = c(w, h, 3))
+
+brightness = wiad:::getBrightness(rgbArray)
