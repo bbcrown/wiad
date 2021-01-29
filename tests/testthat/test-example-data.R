54a6aab6a6e028138c3ada129b6fbde7a77e6ede
bnasr <bijan.s.nasr@gmail.com>
2021-01-28 19:30:13 -0700

    test routines withouth downlaod a big file

--- tests/testthat/test-example-data.R
+++ tests/testthat/test-example-data.R
@@ -3,24 +3,24 @@ context("download example data")
 set.seed(2020)
 
 test_that("test the exampledata function, test", {
-  expect_null(wiad:::getExampleData(mode = 'test'))
+  expect_null(wiad:::getExampleData(mode = 'test', installPath = NULL))
 })
 
 
 test_that("test the exampledata function, install", {
-  expect_null(wiad:::getExampleData(mode = 'install'))
+  expect_null(wiad:::getExampleData(mode = 'install', installPath = NULL))
 })
 
 
 test_that("test the exampledata function, install", {
-  expect_null(wiad:::getExampleData(mode = 'install'))
+  expect_null(wiad:::getExampleData(mode = 'install', installPath = NULL))
 })
 
 test_that("test the exampledata function, update", {
-  expect_null(wiad:::getExampleData(mode = 'update'))
+  expect_null(wiad:::getExampleData(mode = 'update', installPath = NULL))
 })
 
 
 test_that("test the exampledata function, remove", {
-  expect_null(wiad:::getExampleData(mode = 'remove'))
+  expect_null(wiad:::getExampleData(mode = 'remove', installPath = NULL))
 })
