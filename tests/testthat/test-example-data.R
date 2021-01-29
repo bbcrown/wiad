252c35ab6679362af2d0418c0b22811f1bd50d81
bnasr <bijan.s.nasr@gmail.com>
2021-01-28 19:26:01 -0700

    codecov increase

--- tests/testthat/test-example-data.R
+++ tests/testthat/test-example-data.R
@@ -2,6 +2,25 @@ context("download example data")
 
 set.seed(2020)
 
-test_that("test the exampledata function", {
+test_that("test the exampledata function, test", {
   expect_null(wiad:::getExampleData(mode = 'test'))
+})
+
+
+test_that("test the exampledata function, install", {
+  expect_null(wiad:::getExampleData(mode = 'install'))
+})
+
+
+test_that("test the exampledata function, install", {
+  expect_null(wiad:::getExampleData(mode = 'install'))
+})
+
+test_that("test the exampledata function, update", {
+  expect_null(wiad:::getExampleData(mode = 'update'))
+})
+
+
+test_that("test the exampledata function, remove", {
+  expect_null(wiad:::getExampleData(mode = 'remove'))
 })
