7544577a2845bf4706cae191af18f08ffbccbe1f
bnasr <bijan.s.nasr@gmail.com>
2021-01-28 19:41:15 -0700

    simple test

--- tests/testthat/test-example-data.R
+++ tests/testthat/test-example-data.R
@@ -6,21 +6,3 @@ test_that("test the exampledata function, test", {
   expect_null(wiad:::getExampleData(mode = 'test', installPath = NULL))
 })
 
-
-test_that("test the exampledata function, install", {
-  expect_null(wiad:::getExampleData(mode = 'install', installPath = NULL))
-})
-
-
-test_that("test the exampledata function, install", {
-  expect_null(wiad:::getExampleData(mode = 'install', installPath = NULL))
-})
-
-test_that("test the exampledata function, update", {
-  expect_null(wiad:::getExampleData(mode = 'update', installPath = NULL))
-})
-
-
-test_that("test the exampledata function, remove", {
-  expect_null(wiad:::getExampleData(mode = 'remove', installPath = NULL))
-})
