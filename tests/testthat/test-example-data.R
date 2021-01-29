bd17f19e175ad7003c8ed96dd4774eeaea15f76e
bnasr <bijan.s.nasr@gmail.com>
2021-01-28 20:05:05 -0700

    fixed issue

--- tests/testthat/test-example-data.R
+++ tests/testthat/test-example-data.R
@@ -3,6 +3,6 @@ context("download example data")
 set.seed(2020)
 
 test_that("test the exampledata function, test", {
-  expect_null(wiad:::getExampleData(mode = 'test', installPath = NULL))
+  expect_null(wiad:::getExampleData(mode = 'test'))
 })
 
