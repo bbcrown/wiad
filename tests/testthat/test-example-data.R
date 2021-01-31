9270d2ae833cc718f8181b4dc2838065f3947d8b
bnasr <bijan.s.nasr@gmail.com>
2021-01-30 17:03:30 -0700

    test remove

--- tests/testthat/test-example-data.R
+++ tests/testthat/test-example-data.R
@@ -6,3 +6,12 @@ test_that("test the exampledata function, test", {
   expect_null(wiad:::getExampleData(mode = 'test'))
 })
 
+
+test_that("test the exampledata function, remove", {
+  expect_message(wiad:::getExampleData(mode = 'remove'))
+})
+
+test_that("test the exampledata function, remove", {
+  expect_message(wiad:::getExampleData(mode = 'remove'))
+})
+
