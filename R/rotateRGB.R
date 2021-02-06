34521b3383b5a9c4043ef359887d0139b4a1e341
bnasr <bijan.s.nasr@gmail.com>
2021-02-05 18:12:54 -0700

    codecov improv

--- R/rotateRGB.R
+++ R/rotateRGB.R
@@ -9,10 +9,17 @@ rotateRGB <- function (imgMat) {
   if (dim (imgMat) [3] != 3) 
     stop ('matrix must have 3 layers in the 3rd dimension!')
   
-  r <- rotate (imgMat [,,1])
-  g <- rotate (imgMat [,,2])
-  b <- rotate (imgMat [,,3])
+  r <- 
+    rotate (imgMat [,,1])
   
-  rot <- abind (r, g, b, along = 3)
+  g <- 
+    rotate (imgMat [,,2])
+  
+  b <- 
+    rotate (imgMat [,,3])
+  
+  rot <- abind (r, g, b, 
+                along = 3)
+  
   return (rot)
 }
