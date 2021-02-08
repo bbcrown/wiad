1e9a218d3b91198231c57ee82a4d70ca839a62e1
TTRademacher <rademacher.tim@gmail.com>
2021-02-08 17:35:13 -0500

    Solved issue with short second profile, but I still need to test it with longer profile and without a second profile and the issue with the resetting of the par () remains.

--- inst/app/server.R
+++ inst/app/server.R
@@ -682,7 +682,7 @@ shinyServer (function (input, output, session)
       imgDim <- dim (imgtmp)
       
       # set margins and plot are
-      oldpar = par()
+      oldpar = par ()
       par (mar = c (0,0,0,0), xaxs = 'i', yaxs = 'i')
       plot (NA, 
             xlim = c (1, imgDim [2]),
@@ -861,7 +861,7 @@ shinyServer (function (input, output, session)
         }
         
       }
-      par(olpar)
+      #on.exit (par (oldpar))
     })
   
   observeEvent (input$selRed,
@@ -1725,21 +1725,14 @@ shinyServer (function (input, output, session)
       # find pith label's index
       wPith <- tbl [type == 'Pith', no]
       
-      # is there only one profile 
-      if (wPith == nrow (tbl)) {
-        data1 <- tbl
-        nSeries <- 1
-        
-        # if there are two series split them at the pith
-      } else {
-        data2 <- tbl [no > wPith, ]
-        data1 <- tbl [no < wPith, ]
-        nSeries <- 2
-      }
+      # divide data series 
+      data2 <- tbl [no >  wPith, ]
+      data1 <- tbl [no <= wPith, ]
+      nSeries <- 2
       # there is no pith/oldest ring label
     } else {
-      nSeries <- 1
       data1 <- tbl
+      nSeries <- 1
     }
     
     # make sure there are at least three growth increments for each series 
@@ -1756,7 +1749,7 @@ shinyServer (function (input, output, session)
         
         return ()  
       }
-    } else {
+    } else if (nSeries == 2) {
       if (nrow (data1) < 3 & nrow (data2) < 3){
         showModal (strong (
           modalDialog (HTML ("Not enough growth increments to detrend!<br>
@@ -1769,9 +1762,11 @@ shinyServer (function (input, output, session)
         
         return ()  
         
+        # Check whether both series are long enough to be detrended
       } else if ((nrow (data1) < 3 | nrow (data2) < 3)) {
+        if (nrow (data1) < 3) data1 <- data2
         nSeries <- 1
-        data1 <- ifelse (nrow (data1) < 3, data2, data1)
+        rm (data2)
       }
     }
     
