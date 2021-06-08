426c0fbddddbffd075fd564c6e2cfb38f29f796d
Bijan <bijan@bijans-air.lan>
2021-06-08 11:25:17 -0400

    citation onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,20 @@
 globalVariables(names = c('no'))
+
+
+
+
+
+
+.onAttach <- function(libname, pkgname) {
+  citation ="To cite WIAD in publications, please cite both the paper and the R package:
+  
+- Bijan Seyednasrollah, Tim Rademacher, David Basler (2020), WIAD R Package: Wood
+Image and Analysis Dataset
+
+- Rademacher et al., (2021), The Wood Image and Analysis Dataset (WIAD): open-access
+visual analysis tools to advance the ecological data revolution
+
+To see options for other formats, use 'citation('wiad')'."
+  
+  packageStartupMessage(citation)
+}
